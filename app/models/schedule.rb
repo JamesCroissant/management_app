e2cbe42a7316624bbc139cfab33969fd2e33034a
JamesCroissant <yuham1014@gmail.com>
2022-05-01 15:19:07 +0900

    バリデーションを定義

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,8 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :content, presence: true
 end
