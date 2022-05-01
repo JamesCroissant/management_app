8df7369a1d87b2b6d87ee602163043250a52b6b1
JamesCroissant <yuham1014@gmail.com>
2022-05-01 16:08:40 +0900

    showアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'schedules/index'
   devise_for :users
   root to: "schedules#index"
-  resources :schedules , only:[:index, :new, :create]
+  resources :schedules , only:[:index, :new, :create, :show]
 end
  
