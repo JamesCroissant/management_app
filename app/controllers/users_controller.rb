d5e7b73683ff30932848288d73bb5f4c2334b6ee
JamesCroissant <yuham1014@gmail.com>
2022-05-05 13:31:48 +0900

    コントローラー内のshowアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user! , only: :show 
+  
   def show
     @nickname = current_user.nickname
-    @schedules = current_user.schedules
+    @schedules = current_user.schedules.order(start_time: :asc)
   end
 end
