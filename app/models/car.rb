0d0f469cc8314ba96981728617bee430e0c22fef
JamesCroissant <yuham1014@gmail.com>
2022-05-06 17:29:33 +0900

    バリデーションを追加

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_one :schedule
+
+  validates :car_name, presence: true
+  validates :car_number, presence: true
+  validates :car_model, presence: true
 end
