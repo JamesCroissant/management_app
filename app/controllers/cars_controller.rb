bf7c4e35bbc20c8ee3d29c4eca3206402e689c0c
JamesCroissant <yuham1014@gmail.com>
2022-05-06 17:29:23 +0900

    createアクションを定義

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,22 @@
 class CarsController < ApplicationController
+
   def new
     @car = Car.new
   end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def car_params
+    params.require(:car).permit(:car_name, :car_number, :car_model).merge(user_id: current_user.id)
+  end
+
 end
