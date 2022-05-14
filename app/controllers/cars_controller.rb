a378877d97efc4e228243e90bde13f56bff1f66b
JamesCroissant <yuham1014@gmail.com>
2022-05-15 00:17:08 +0900

    各アクションをコントローラーに定義

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,8 @@
 class CarsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-
+  before_action :set_car, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
+  
   def index
     @cars = Car.all
   end
@@ -19,7 +21,22 @@ class CarsController < ApplicationController
   end
 
   def show
-    @car = Car.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to car_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to root_path
   end
 
   private
@@ -27,4 +44,13 @@ class CarsController < ApplicationController
   def car_params
     params.require(:car).permit(:car_name, :car_number, :car_model).merge(user_id: current_user.id)
   end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless @car.user_id == current_user.id
+  end
+
 end
