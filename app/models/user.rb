9c32020882734a324a2ef963434ce109a96df621
JamesCroissant <yuham1014@gmail.com>
2022-05-17 19:49:04 +0900

    userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :schedules
+
+  validates :nickname, presence: true, uniqueness: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字の両方を含める必要があります' }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+
+  validates :birthday, presence: true
+  validates :occupation, presence: true
+  validates :birthday, presence: true
+  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' },
+                           length: { minimum: 10, maximum: 11 }, allow_blank: true
+
 end
