cb71dfa7ac16a3940844f80bb3094e7b3289e7be
JamesCroissant <yuham1014@gmail.com>
2022-05-17 19:48:17 +0900

    carモデルのバリデーション追加実装

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ApplicationRecord
   has_one :schedule
 
   validates :car_name, presence: true
-  validates :car_number, presence: true
+  validates :car_number, numericality: { only_integer: true, message: 'is invalid. Input only number' },
+                         length: { is: 4 }, allow_blank: true
   validates :car_model, presence: true
 end
