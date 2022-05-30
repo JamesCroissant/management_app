6753588b84572a134495facc8db0c361b940ed3f
JamesCroissant <yuham1014@gmail.com>
2022-05-30 16:40:09 +0900

    新規登録ページのフロント実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字の両方を含める必要があります' }, allow_blank: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+  with_options allow_blank: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
     validates :family_name
     validates :first_name
   end
 
+  validates :family_name, presence: true
+  validates :first_name, presence: true
   validates :birthday, presence: true
   validates :occupation, presence: true
   validates :phone_number, presence: true                    # 直す
